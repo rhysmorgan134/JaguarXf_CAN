0958cb71608bb41e10be2e45928f41fea6cecd24
Rhys Morgan <rhys1802@hotmail.co.uk>
2019-11-02 18:08:51 +0000

    added functional backend server, html files, and client javascript

--- app/html/app.js
+++ app/html/app.js
@@ -1,17 +1,24 @@
-var socket = io.connect('http://192.168.0.88:3000')
+var socket = io.connect('http://localhost:3000')
 
 buttonPress = function(name, event) {
-    console.log(event)
+    socket.emit("action", {"type": name, "func":"pressed"})
+}
+
+buttonRel = function(name, event) {
+    socket.emit("action", {"type": name, "func":"rel"})
 }
 
 document.addEventListener("DOMContentLoaded", onDomReadyHandler);
 
 function onDomReadyHandler(event) {
     socket.on('status', (data) => {
+        console.log(data)
         for (var k in data) {
-            if (data[k] === 1) {
+            if (data[k] > 0) {
+                console.log("led on")
                 document.getElementById(k).style.backgroundColor =  '#FF9933';
             } else {
+                console.log("led of")
                 document.getElementById(k).style.backgroundColor =  '#161625';
             }
         }
