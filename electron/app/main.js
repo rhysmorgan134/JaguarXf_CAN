0617f149d6ddb4e0985e6127fd5b5ae8cf1261d7
rhys_m <rhys1802@hotmail.co.uk>
2020-06-05 10:52:27 +0100

    updated server.js to handle intelligent auto

--- electron/app/main.js
+++ electron/app/main.js
@@ -17,12 +17,16 @@ function createWindow () {
       nodeIntegration: true
     }
   })
+
+//mainWindow.webContents.session.clearCache(function(){}
 // process.noAsar=true;
   // and load the index.html of the app.
   win.loadURL('http://localhost:3000')
+//const ses = win.webContents.session.clearCache(function() {
+//});
   win.removeMenu()
   // Open the DevTools.
-  win.webContents.openDevTools()
+//  win.webContents.openDevTools()
 
   // Emitted when the window is closed.
   win.on('closed', () => {
